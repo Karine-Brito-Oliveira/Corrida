40ba452a5f00f5035ce0b5aec108666631b00736
Karine-Brito-Oliveira <karineebrito508@gmail.com>
2025-08-13 14:16:48 -0300

    Add files via upload

---
+++ karine.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    int evento = 5;
+    char corrida  = 'c';
+    float tempo = 27.25;
+    
+    printf("O melhor tempo da eliminatoria %C", corrida);
+    printf("\n do %d foi %f" , evento, tempo);
+}
